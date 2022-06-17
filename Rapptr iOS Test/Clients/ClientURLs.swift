0317a23f08707b8bbc7fb6edf2f26c3d7b47e115
Kevin Wu <Kevin Wu>
2022-06-17 15:40:10 -0400

    feat(Clients): add clientURLs file

---
+++ Rapptr iOS Test/Clients/ClientURLs.swift
@@ -0,0 +1,17 @@
+//
+//  ClientURLs.swift
+//  Rapptr iOS Test
+//
+//  Created by Jack on 6/17/22.
+//
+
+import Foundation
+
+struct ClientURLs {
+    static let dev :String = "http://dev.rapptrlabs.com/"
+    
+    struct Paths {
+        static let login = "Tests/scripts/login.php"
+        static let chatLog = "Tests/scripts/chat_log.php"
+    }
+}
