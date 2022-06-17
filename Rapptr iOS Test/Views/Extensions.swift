8550ccb3f41557755bf7a40333e871990f002a30
Kevin Wu <Kevin Wu>
2022-06-17 17:24:08 -0400

    feat(Navigation): change text color to white

--- Rapptr iOS Test/Views/Extensions.swift
+++ Rapptr iOS Test/Views/Extensions.swift
@@ -9,10 +9,11 @@ import Foundation
 import UIKit
 
 extension UINavigationController {
-    func setNavigationBarColor(_ color: UIColor) {
+    func setNavigationBarColor(_ color: UIColor, textColor: UIColor) {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = color
+        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: textColor]
         navigationBar.standardAppearance = appearance
         navigationBar.scrollEdgeAppearance = navigationBar.standardAppearance
     }
