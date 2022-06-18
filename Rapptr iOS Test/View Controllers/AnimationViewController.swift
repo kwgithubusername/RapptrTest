836a34707cf42457634c155feaf5981ec6ef10dc
Kevin Wu <Kevin Wu>
2022-06-17 22:20:16 -0400

    refactor(Animation): move draggable image view to separate file

--- Rapptr iOS Test/View Controllers/AnimationViewController.swift
+++ Rapptr iOS Test/View Controllers/AnimationViewController.swift
@@ -72,22 +72,3 @@ class AnimationViewController: UIViewController {
         view.addSubview(rainbow)
     }
 }
-
-class DraggableImageView: UIImageView {
-      var beganPoint: CGPoint? = nil
-      var originCenter: CGPoint? = nil
-      override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-           if let position = touches.first?.location(in: superview){
-                beganPoint = position
-                originCenter = center
-           }
-      }
-      override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-      }
-      override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-           if let position = touches.first?.location(in: superview){
-                let newPosition = CGPoint(x: position.x - (beganPoint?.x)!, y: position.y - (beganPoint?.y)!)
-                center = CGPoint(x: (originCenter?.x)! + newPosition.x, y: (originCenter?.y)! + newPosition.y)
-           }
-      }
-}
